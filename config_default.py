c9426d77aa0a6cde6a90d5d69189e661ba8bef3a
Evan Mu <190567268@qq.com>
2016-09-18 22:08:55 +0800

    add config.py

---
+++ config_default.py
@@ -0,0 +1,13 @@
+# config_default.py
+
+configs = {
+    'db': {
+        'host': '127.0.0.1',
+        'port': '8080',
+        'user': 'www-data',
+        'database': 'yourdb',
+    },
+    'session': {
+        'secret': 'THESECRET'
+    }
+}
