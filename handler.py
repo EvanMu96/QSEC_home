3823c1dd7e5f37933e1f1354dae6f4506663bb0b
Evan Mu <190567268@qq.com>
2016-09-22 18:17:43 +0800

    添加获取user api

--- handler.py
+++ handler.py
@@ -9,8 +9,22 @@ from model import User, Comment, Article
 
 @get('/')
 async def index(request):
-    users = await User.findAll()
+    summary = 'test string'
+    articles = [
+        Article(id='1', name='Test Blog', summary=summary, create_at=time.time()-120),
+        Article(id='1', name='Test Blog', summary=summary, create_at=time.time() - 120),
+        Article(id='1', name='Test Blog', summary=summary, create_at=time.time() - 120),
+    ]
     return {
-        '__template__': 'test.html',
-        'user': users
+        '__template__': 'blog.html',
+        'aricles': articles
     }
+
+
+@get('/api/users')
+def api_get_users():
+    users = yield from User.findAll(orderBy='create_at desc')
+    for u in users:
+        u.password = '******'
+    return dict(users)
+
